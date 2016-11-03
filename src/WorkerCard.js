9f01b1105dc2bd05e2d15c0cce0f8f9920014c3f
Andy <andywhinh@gmail.com>
2016-11-03 03:07:08 -0700

    Meh

--- src/WorkerCard.js
+++ src/WorkerCard.js
@@ -1,43 +1,70 @@
 import React, { Component } from 'react';
-import {Card, CardActions, CardHeader, CardText, FlatButton} from 'material-ui';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui';
 import { LineChart, XAxis, YAxis, ToolTip, CartesianGrid, Line } from 'recharts';
 
 const data = [{
-  name: "10:00AM",
-  heartRate: 70,
-  temperature: 98
-}, {
-  name: "10:30AM",
-  heartRate: 90,
-  temperature: 98
-}, {
-  name: "11:00AM",
-  heartRate: 800,
-  temperature: 98
-}, {
-  name: "11:30AM",
-  heartRate: 120,
-  temperature: 98
+  name: "Jeff",
+  value: [{
+    time: "10:00AM",
+    heartRate: 70,
+    temperature: 98,
+    steps: 100
+  }, {
+    time: "10:30AM",
+    heartRate: 90,
+    temperature: 98,
+    steps: 500
+  }, {
+    time: "11:00AM",
+    heartRate: 50,
+    temperature: 98,
+    steps: 670
+  }, {
+    time: "11:30AM",
+    heartRate: 120,
+    temperature: 98,
+    steps: 800
+  }]
+},
+{
+  name: "Steve",
+  value: [{
+    time: "10:00AM",
+    heartRate: 70,
+    temperature: 98,
+    steps: 100
+  }, {
+    time: "10:30AM",
+    heartRate: 70,
+    temperature: 98,
+    steps: 500
+  }, {
+    time: "11:00AM",
+    heartRate: 50,
+    temperature: 98,
+    steps: 670
+  }, {
+    time: "11:30AM",
+    heartRate: 60,
+    temperature: 98,
+    steps: 800
+  }]
 }];
 
 
 export default class WorkerCard extends Component {
+
   render() {
-    return (
-      <Card>
+    var cards = data.map(function(stuff) {
+      return <Card>
         <CardHeader
-          title="Without Avatar"
-          subtitle="Subtitle"
+          title={stuff.name}
           actAsExpander={true}
           showExpandableButton={true}
         />
-        <CardActions>
-          <FlatButton label="Action1" />
-          <FlatButton label="Action2" />
-        </CardActions>
         <CardText expandable={true}>
-          <LineChart width={600} height={300} data={data} margin={{top: 5, right:30, left:20, bottom: 5}}>
-            <XAxis dataKey="name"/>
+          <LineChart width={600} height={300} data={stuff.value} margin={{top: 5, right:30, left:20, bottom: 5}}>
+            <XAxis dataKey="time"/>
             <YAxis />
             <ToolTip />
             <CartesianGrid strokeDasharray="3 3" />
@@ -46,6 +73,9 @@ export default class WorkerCard extends Component {
           </LineChart>
         </CardText>
       </Card>
+    });
+    return (
+      <div>{cards}</div>
     );
   }
 }
