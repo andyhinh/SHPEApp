f7bc49eb3feb1834356677963876d2f8a492f3ff
Juan Carrillo <oFlyingTurtleo@Juans-MacBook-Pro.local>
2016-11-03 06:56:30 -0400

    Seperated mock data for for readibility

--- src/WorkerCard.js
+++ src/WorkerCard.js
@@ -1,69 +1,19 @@
 import React, { Component } from 'react';
-import {Card, CardActions, CardHeader, CardText} from 'material-ui';
+import {Card, CardHeader, CardText} from 'material-ui';
 import { LineChart, XAxis, YAxis, ToolTip, CartesianGrid, Line } from 'recharts';
-
-const data = [{
-  name: "Jeff",
-  value: [{
-    time: "10:00AM",
-    heartRate: 70,
-    temperature: 98,
-    steps: 100
-  }, {
-    time: "10:30AM",
-    heartRate: 90,
-    temperature: 98,
-    steps: 500
-  }, {
-    time: "11:00AM",
-    heartRate: 50,
-    temperature: 98,
-    steps: 670
-  }, {
-    time: "11:30AM",
-    heartRate: 120,
-    temperature: 98,
-    steps: 800
-  }]
-},
-{
-  name: "Steve",
-  value: [{
-    time: "10:00AM",
-    heartRate: 70,
-    temperature: 98,
-    steps: 100
-  }, {
-    time: "10:30AM",
-    heartRate: 70,
-    temperature: 98,
-    steps: 500
-  }, {
-    time: "11:00AM",
-    heartRate: 50,
-    temperature: 98,
-    steps: 670
-  }, {
-    time: "11:30AM",
-    heartRate: 60,
-    temperature: 98,
-    steps: 800
-  }]
-}];
-
+import { workers } from './mock/data';
 
 export default class WorkerCard extends Component {
-
   render() {
-    var cards = data.map(function(stuff) {
+    var cards = workers.map(function(worker) {
       return <Card>
         <CardHeader
-          title={stuff.name}
+          title={worker.name}
           actAsExpander={true}
           showExpandableButton={true}
         />
         <CardText expandable={true}>
-          <LineChart width={600} height={300} data={stuff.value} margin={{top: 5, right:30, left:20, bottom: 5}}>
+          <LineChart width={600} height={300} data={worker.value} margin={{top: 5, right:30, left:20, bottom: 5}}>
             <XAxis dataKey="time"/>
             <YAxis />
             <ToolTip />
---
+++ src/mock/data.js
@@ -0,0 +1,48 @@
+export const workers = [{
+  name: "Jeff",
+  value: [{
+    time: "10:00AM",
+    heartRate: 70,
+    temperature: 98,
+    steps: 100
+  }, {
+    time: "10:30AM",
+    heartRate: 90,
+    temperature: 98,
+    steps: 500
+  }, {
+    time: "11:00AM",
+    heartRate: 50,
+    temperature: 98,
+    steps: 670
+  }, {
+    time: "11:30AM",
+    heartRate: 120,
+    temperature: 98,
+    steps: 800
+  }]
+},
+{
+  name: "Steve",
+  value: [{
+    time: "10:00AM",
+    heartRate: 70,
+    temperature: 98,
+    steps: 100
+  }, {
+    time: "10:30AM",
+    heartRate: 70,
+    temperature: 98,
+    steps: 500
+  }, {
+    time: "11:00AM",
+    heartRate: 50,
+    temperature: 98,
+    steps: 670
+  }, {
+    time: "11:30AM",
+    heartRate: 60,
+    temperature: 98,
+    steps: 800
+  }]
+}];
