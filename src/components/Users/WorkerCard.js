0b52b818612e3b8dc3528f645408005b79f15396
Andy <andywhinh@gmail.com>
2016-11-04 05:50:15 -0700

    Logic coloring

--- src/components/Users/WorkerCard.js
+++ src/components/Users/WorkerCard.js
@@ -13,8 +13,8 @@ const WorkerCard = (props) => (
       actAsExpander={true}
       showExpandableButton={true}
       style={props.worker.value[props.worker.value.length-1].heartRate > 150
-              || props.worker.value[props.worker.value.length-1].temperature > 99
-              || props.worker.value[props.worker.value.length-1].temperature < 96
+              || props.worker.value[props.worker.value.length-1].temperature > 99.5
+              || props.worker.value[props.worker.value.length-1].temperature < 95.5
         ? {backgroundColor: "red", paddingLeft: 90}
         : props.worker.value[props.worker.value.length-1].heartRate > 120
           ? {backgroundColor: "orange", paddingLeft: 90}
